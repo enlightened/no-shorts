b99b2f12246b760c210b9afe29c72ae52dd72ddc
aydinyal <aydincodes@gmail.com>
2023-02-18 22:40:25 -0500

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMSubtreeModified", function(event){
+
+    const mini = document.querySelector("#items > ytd-mini-guide-entry-renderer:nth-child(2)")
+    if (mini) {
+        mini.style.display = "none"
+    }
+    const btn = document.querySelector("#items > ytd-guide-entry-renderer:nth-child(2)")
+    if (btn) {
+        btn.style.display = "none"
+    }
+    const videos = document.querySelector("#contents > ytd-rich-section-renderer")
+    if (videos) {
+        videos.style.display = "none"
+    }
+
+    if (document.location.href.includes("shorts")) {
+        (async () => {
+            await chrome.runtime.sendMessage({greeting: "shorts"});
+          })();
+    }
+});
